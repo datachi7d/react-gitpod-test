9efbd281132af02c0231c8fc07cfc9865b29cf45
datachi7d <seandtkelly@gmail.com>
2020-12-05 09:11:38 +0000

    Basic form with react

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+// eslint-disable-next-line
+import { Form, TextField, SelectField, SubmitButton, LeafletMap } from './FormElements';
+import * as Yup from 'yup';
+import "./App.css"
+import 'leaflet/dist/leaflet.css';
+import 'milligram/dist/milligram.css';
+
+const formSchema = {
+    name: {
+        type: "text",
+        label: "Name",
+        required: true
+    },
+    email: {
+        type: "email",
+        label: "Email",
+        required: true
+    },
+    positon: {
+        type: "map",
+        options: {
+            lat: -42.0,
+            lon: 173.0,
+            zoom: 5
+        }
+    },
+    submit: {
+        type: "submit",
+        label: "Submit",
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [formData, setFormData] = useState({});
+    const [validationSchema, setValidationSchema] = useState({});
+
+    useEffect(() => {   
+        initForm(formSchema);
+    }, []);
+
+    const initForm = (formSchema) => {
+        let _formData = {};
+        let _validationSchema = {};
+
+        for(var key of Object.keys(formSchema)){
+            _formData[key] = "";
+
+            if(formSchema[key].type === "text"){
+                _validationSchema[key] = Yup.string();
+            }else if(formSchema[key].type === "email"){
+                _validationSchema[key] = Yup.string().email()
+            }else if(formSchema[key].type === "select"){
+                _validationSchema[key] = Yup.string().oneOf(formSchema[key].options.map(o => o.value));
+            }
+
+            if(formSchema[key].required){
+                _validationSchema[key] = _validationSchema[key].required('Required');
+            }
+        }
+
+        setFormData(_formData);
+        setValidationSchema(Yup.object().shape({ ..._validationSchema }));
+    }
+
+    const getFormElement = (elementName, elementSchema) => {
+        const props = {
+            name: elementName,
+            label: elementSchema.label,
+            options: elementSchema.options
+        };
+
+        if (elementSchema.type === "text" || elementSchema.type === "email") {
+            return <TextField {...props} />
+        }
+        if (elementSchema.type === "select") {
+            return <SelectField  {...props} />
+        }
+        if(elementSchema.type === "map") {
+            return <LeafletMap {...props} />
+        }
+        if(elementSchema.type === "submit") {
+            return <SubmitButton {...props} />
+        }
+
+
+    }
+
+    const onSubmit = (values, { setSubmitting, resetForm, setStatus }) => {
+        console.log(values);
+        setSubmitting(false);
+    }
+
+    return (
+        <div className="App">
+            <Form
+                enableReinitialize
+                initialValues={formData}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+            >
+                {Object.keys(formSchema).map( (key, ind) => (
+                    <div key={key}>
+                        {getFormElement(key, formSchema[key])}
+                    </div>
+                ))}
+            </Form>
+        </div>
+    );
 }
 
 export default App;
---
+++ hello-world/src/FormElements.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+    Formik,
+    Form as FormikForm,
+    Field,
+    ErrorMessage,
+    useFormikContext,
+// eslint-disable-next-line
+    useField,
+// eslint-disable-next-line
+    useFormik
+} from 'formik';
+
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup
+} from 'react-leaflet';
+
+export function Form(props) {
+    return (
+        <Formik
+            {...props}
+        >
+            <FormikForm className="needs-validation" novalidate="">
+                {props.children}
+            </FormikForm>
+        </Formik>)
+}
+
+export function TextField(props) {
+    const { name, label, placeholder, ...rest } = props
+    return (
+        <>
+            {label && <label for={name}>{label}</label>}
+            <Field
+                className="form-control"
+                type="text"
+                name={name}
+                id={name}
+                placeholder={placeholder || ""} 
+                {...rest}
+            />
+            <ErrorMessage name={name} render={msg => <div style={{ color: 'red' }} >{msg}</div>} />
+        </>
+    )
+}
+
+export function SelectField(props) {
+    const { name, label, options } = props
+    return (
+        <>
+            {label && <label for={name}>{label}</label>}
+            <Field
+                as="select"
+                id={name}
+                name={name}
+            >
+                <option value="" >Choose...</option>
+                {options.map((optn, index) => <option value={optn.value} label={optn.label || optn.value} />)}
+            </Field>
+            <ErrorMessage name={name} render={msg => <div style={{ color: 'red' }} >{msg}</div>} />
+        </>
+    )
+}
+
+export function SubmitButton(props){
+    const { label, ...rest } = props;
+    const { isSubmitting } = useFormikContext();
+    
+    return (
+        <button type="submit" {...rest} disabled={isSubmitting}>{label}</button>
+    )
+}
+
+export function LeafletMap(props){
+    const { lat, lon, zoom } = props.options
+    return (
+        <MapContainer center={[lat, lon]} zoom={zoom}>
+        <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        </MapContainer>
+    )
+}
