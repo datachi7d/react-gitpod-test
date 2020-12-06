7c2f6978b0b847d55398add4b7170bddbe2780c7
datachi7d <seandtkelly@gmail.com>
2020-12-06 03:51:52 +0000

    Fix ports for gitpod and update documentation

--- back-end/app.js
+++ back-end/app.js
@@ -22,8 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.set('port', process.env.BACKEND_PORT || 3001);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
